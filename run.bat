d3d4998bab11a797863236d60e30f86b1419dfcc
Valmiki Vishwakarma <you@example.com>
2022-12-20 13:16:24 +0530

    Pushing code changes.

---
+++ run.bat
@@ -0,0 +1,7 @@
+
+pytest -v -s  --html=Reports\reportDec18.html testcases/test_login_ddt.py --browser chrome
+
+REM This is just the sample comment 
+REM pytest -v -s  --html=Reports\reportDec18.html testcases/test_login_ddt.py --browser chrome
+REM pytest -v -s  --html=Reports\reportDec18.html testcases/test_login_ddt.py --browser edge
+REM REMOVABLE COMMENT
